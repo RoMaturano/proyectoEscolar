072cd2d8c515d49e1c2f10409b3298711c39cbf0
Rocio <romaturano96@gmail.com>
2023-06-17 16:31:37 -0300

    i

--- institutoDeanF/client/src/components/Detail/Detail.jsx
+++ institutoDeanF/client/src/components/Detail/Detail.jsx
@@ -1,13 +1,64 @@
-
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Link, useParams } from "react-router-dom/cjs/react-router-dom.min"
+import style from "./Detail.module.css"
 
 
 
 
 const Detail=()=>{
+const {id}=useParams()
+const [detail,setDetail]=useState({})
+
+useEffect(()=>{
+axios.get(`http://localhost:3001/student/student/${id}`)
+.then ((data)=>{
+    // console.log(data.data,"esto es data");
+    // console.log(data.data.id,"esto es data.data.id");
+    if(data.data.id){
+        setDetail(data.data)
+    }else{
+        window.alert("there is not student whit this id")
+    }
+})
+},[id])
+
 
 return(
     <div>
-        <h1>DETAILLLL</h1>
+        <div className={style.titleANDname}>
+        <h1>DETAIL OF STUDENT: </h1>
+        <h1>{detail.name + " " + detail.lastName }</h1>
+       
+        </div>
+
+        <div className={style.containerImgandLabel}>
+        <div className={style.contenedor}>
+            <label className={style.label}>ID:</label>
+            <p className={style.labelNumbers}> {detail.id}</p>
+            <label className={style.label}>DNI: </label>
+            <p className={style.labelNumbers}>{detail.dni}</p>
+            <label className={style.label}>PHONE: </label>
+            <p className={style.labelNumbers}>{detail.phone}</p>
+            <label className={style.label}>EMAIL:</label>
+            <p className={style.labelNumbers}> {detail.email}</p>
+            <label className={style.label} >FECHA DE NACIMIENTO</label>
+            <p className={style.labelNumbers}>{detail.birthDate}</p>
+            <label className={style.label} >MATERIAS</label>
+            <p className={style.labelNumbers}>{detail.Subjects?.map(el=>el+" ")}</p>
+            <label className={style.label} >ESCOLAR CYCLE</label>
+            <p className={style.labelNumbers}>{detail.EscolarCycle}</p>
+        </div>
+
+        <div>
+            <img src={detail.image} alt= "img not found" className={style.img}></img>
+        </div>
+        </div>
+
+        <div className={style.buttomDiv}>
+            <Link to="/"><button className={style.butom}>Home</button></Link>
+            <Link to="/students"><button className={style.butom}>Return</button></Link>
+        </div>
     </div>
 )
 
