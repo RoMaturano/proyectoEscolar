f10017825bb6f5568d914e980b064f2ab64b0a32
Rocio <romaturano96@gmail.com>
2023-06-19 17:58:16 -0300

    estilos css

--- institutoDeanF/client/src/components/Home/Home.jsx
+++ institutoDeanF/client/src/components/Home/Home.jsx
@@ -4,12 +4,7 @@ import NavBar from "../Nav/NavBar"
 
 
 const Home=()=>{
-    // const images = [
-    //     'https://lh3.googleusercontent.com/p/AF1QipP4Y8UTv02xM8_g0QYt5dVXfWYbGDn3SEL_0J8b=s680-w680-h510',
-    //     'https://www.institutodeanfunes.edu.ar/wp-content/uploads/2017/11/Instituto-Dean-Funes.jpg',
-    //     'https://1.bp.blogspot.com/-B1LP1Ocws2w/Wt4KTeNknDI/AAAAAAAAC4E/R0qYIteC2koh6X5OhBtPRiisBUV0eh3VACLcBGAs/s1600/IMG_20180419_151011521.jpg',
-       
-    //   ];
+   
     
 return (
     <body className={style.divcontainer}>
@@ -19,7 +14,7 @@ return (
             </div>
            
             <div className={style.div1}>
-                <Link to="/"><h2 className={style.t1}>INSTITUCION EDUCATIVA</h2></Link>
+                <Link to="/"><h2 className={style.t1}>INSTITO PRIVADO</h2></Link>
                 <Link to="/"><h2 className={style.t2}>DEAN FUNES</h2></Link>
             </div>
             <div className={style.div2}>
